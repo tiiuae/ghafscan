36c5b4dc47d34e1e3d6167ea7906bfe0e04cffe7
Henri Rosten <henri.rosten@unikie.com>
2023-11-27 15:36:07 +0200

    Update csvdiff and sbomnix inputs
    Signed-off-by: Henri Rosten <henri.rosten@unikie.com>

--- sbomnix.nix
+++ sbomnix.nix
@@ -3,5 +3,5 @@
 # SPDX-License-Identifier: Apache-2.0
 builtins.fetchGit {
   url = "https://github.com/tiiuae/sbomnix.git";
-  rev = "24007b6ad064c2f6055ffb4e59a8491b08357353";
+  rev = "b920aa90ee291defa00e1757d81399f37c76c853";
 }
