01b4c0afd50a1fee1397bd5c855f269484086484
Henri Rosten <henri.rosten@unikie.com>
2023-11-27 16:06:21 +0200

    Remove unused sbomnix.nix
    Signed-off-by: Henri Rosten <henri.rosten@unikie.com>

--- sbomnix.nix
+++
@@ -1,7 +0,0 @@
-# SPDX-FileCopyrightText: 2023 Technology Innovation Institute (TII)
-#
-# SPDX-License-Identifier: Apache-2.0
-builtins.fetchGit {
-  url = "https://github.com/tiiuae/sbomnix.git";
-  rev = "b920aa90ee291defa00e1757d81399f37c76c853";
-}
