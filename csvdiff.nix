53f666e10c00ef747f19ea9500c147e3a572098c
Henri Rosten <henri.rosten@unikie.com>
2023-12-01 15:27:10 +0200

    Remove unused csvdiff.nix
    Signed-off-by: Henri Rosten <henri.rosten@unikie.com>

--- csvdiff.nix
+++
@@ -1,7 +0,0 @@
-# SPDX-FileCopyrightText: 2023 Technology Innovation Institute (TII)
-#
-# SPDX-License-Identifier: Apache-2.0
-builtins.fetchGit {
-  url = "https://github.com/tiiuae/ci-public.git";
-  rev = "205b2f3e497c16bd094372e98b9c84d8bc307ec3";
-}
