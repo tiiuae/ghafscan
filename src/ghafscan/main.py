8118942533be01530b7de7a97511b0540a69b7ba
Henri Rosten <henri.rosten@unikie.com>
2024-04-09 12:25:54 +0300

    Flake update
    Signed-off-by: Henri Rosten <henri.rosten@unikie.com>

--- src/ghafscan/main.py
+++ src/ghafscan/main.py
@@ -4,7 +4,7 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-# pylint: disable=invalid-name, too-many-instance-attributes
+# pylint: disable=too-many-instance-attributes
 # pylint: disable=too-many-statements, too-many-locals
 
 """ Run and summarize vulnerability scans for flake targets """
