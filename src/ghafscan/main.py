00fd122a702cf58b1852ff29f02bcf5e49711b21
Henri Rosten <henri.rosten@unikie.com>
2025-09-24 15:14:13 +0300

    Scan buildtime depenencies
    Signed-off-by: Henri Rosten <henri.rosten@unikie.com>

--- src/ghafscan/main.py
+++ src/ghafscan/main.py
@@ -598,9 +598,7 @@ def main():
         LOG.warning("Ignoring inaccessible whitelist: %s", whitelist.as_posix())
         whitelist = None
     for target in args.target:
-        scanner.scan_target(
-            target[0], buildtime=False, nixprs=True, whitelist=whitelist
-        )
+        scanner.scan_target(target[0], nixprs=True, whitelist=whitelist)
     scanner.report(args.outdir)
 
 
